4d2dbdc762bf099b83dece6024b5e53040b6c55c
nxf56462 <nxf56462@NXL71572>
2019-08-16 17:03:02 +0300

    Implementig loading and rendering bitmaps

---
+++ EU4 Game Editing Tool WinForm/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace EU4_Game_Editing_Tool_WinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.MapPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(12, 12);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(98, 29);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open Image";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.OpenFileButtonClick);
+            // 
+            // MapPictureBox
+            // 
+            this.MapPictureBox.Location = new System.Drawing.Point(12, 47);
+            this.MapPictureBox.Name = "MapPictureBox";
+            this.MapPictureBox.Size = new System.Drawing.Size(1334, 470);
+            this.MapPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.MapPictureBox.TabIndex = 1;
+            this.MapPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 529);
+            this.Controls.Add(this.MapPictureBox);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "MainForm";
+            this.Text = "EU4 Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.MapPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.PictureBox MapPictureBox;
+    }
+}
+
---
+++ EU4 Game Editing Tool WinForm/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EU4_Game_Editing_Tool_WinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+            Graphics graphics = e.Graphics;
+            graphics.DrawString("some text", this.Font, Brushes.AliceBlue, 0, 0);
+            BackColor = Color.Black;
+        }
+
+        public OpenFileDialog loadImageDialog = new OpenFileDialog()
+        {
+            Filter = "Image Files (*.bmp)|*.bmp",
+            InitialDirectory = "C:/Users/nxf56462/Downloads/Phoenix 3 - DW 5.2/Phoenix"
+        };
+
+        private void OpenFileButtonClick(object sender, EventArgs args)
+        {
+            if(loadImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Uri filePath = new Uri(loadImageDialog.FileName);
+                    Image provinces = Image.FromFile(loadImageDialog.FileName);
+                    MapPictureBox.Image = provinces;
+                    
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+
+            }
+        }
+
+    }
+}
