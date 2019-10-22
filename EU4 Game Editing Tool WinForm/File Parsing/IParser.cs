6ea65385c2ad5be61440bb005108d4314a5278e5
nxf56462 <nxf56462@NXL71572>
2019-10-22 13:27:58 +0300

    Created abstract class for basic parsing functionality

---
+++ EU4 Game Editing Tool WinForm/File Parsing/IParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU4_Game_Editing_Tool_WinForm.File_Parsing
+{
+    interface IParser
+    {
+        TextNode ParseFile(string filename);
+    }
+}
---
+++ EU4 Game Editing Tool WinForm/File Parsing/TextNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU4_Game_Editing_Tool_WinForm.File_Parsing
+{
+    class TextNode
+    {
+        public List<TextNode> mChildNodes = new List<TextNode>();
+
+        public List<TextElement> mChildElements = new List<TextElement>();
+
+        public List<String> mChildValues = new List<string>();
+
+        public String mValue;
+    }
+}
