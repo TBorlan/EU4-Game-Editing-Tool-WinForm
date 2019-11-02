1d1c3f7658cd308941f24f9eac21bf291e505760
nxf56462 <nxf56462@NXL71572>
2019-11-02 14:22:44 +0200

    [ISSUE-27] Added some file parsing things.Changed other things in Game Data

---
+++ EU4 Game Editing Tool WinForm/FileParsing/FileReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EU4_Game_Editing_Tool_WinForm.FileParsing
+{
+    class FileReader : IFileReader
+    {
+        protected virtual string[] GetTextTokens()
+        {
+            return null;
+        }
+
+        protected virtual string[] GetCsvTokens()
+        {
+            return null;
+        }
+
+        private string _mFilePath;
+
+        public string mFilePath
+        {
+            set
+            {
+                if (File.Exists(value))
+                {
+                    _mFilePath = value;
+                }
+                else
+                {
+                    string message = "File " + Path.GetFileName(value) + " does not exist";
+                    throw new Exception(message);
+                }
+            } 
+        }
+
+        public string[] GetTokens(string filePath)
+        {
+            
+        }
+    }
+}
---
+++ EU4UnitTests/FileReaderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace EU4UnitTests
+{
+    [TestClass]
+    class FileReaderTests
+    {
+        [TestMethod]
+        public void GetTokens_InvalidFileExtension_RaiseException()
+        {
+
+        }
+        [TestMethod]
+        public void GetTokens_FileDoesNotExist_RaiseException()
+        {
+
+        }
+        [TestMethod]
+        public void GetTokens_TextFileEmpty_EmptyListReturn()
+        {
+
+        }
+        [TestMethod]
+        public void GetTokens_TextFileWithText_ReturnsCorrectTokens()
+        {
+
+        }
+        [TestMethod]
+        public void GetTokens_TextFileWithQuotedString_ReturnsSingleString()
+        {
+
+        }
+        [TestMethod]
+        public void GetTokens_TextFileWithComments_ReturnsListWithoutComments()
+        {
+
+        }
+        [TestMethod]
+        public void GetTokens_TextFileMissingSpaceBetweenControlTokens_ReturnsSplitTokens()
+        {
+
+        }
+
+
+    }
+}
