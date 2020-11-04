a579e31cf48437f014ff00995500fd2377139312
TBorlan <tudor.borlan@gmail.com>
2020-11-04 14:18:51 +0200

    [EU4GET-16] Removed unused code

--- EU4 Game Editing Tool WinForm/SelectionManager.cs
+++ EU4 Game Editing Tool WinForm/SelectionManager.cs
@@ -47,40 +47,6 @@ namespace EU4_Game_Editing_Tool_WinForm
             }
         }
 
-        public Point[] GetDifferenceLine(Point[] line1, Point[] line2)
-        {
-            Point difference;
-            Point[] result = new Point[2];
-            if ((line1[0].X != line2[0].X) || (line1[0].Y != line2[0].Y))
-            {
-                difference = new Point(line1[0].X - line2[0].X, line1[0].Y - line2[0].Y);
-                if(difference.X < 0 || difference.Y < 0)
-                {
-                    result[1] = new Point(line2[0].X, line2[0].Y);
-                }
-                else
-                {
-                    result[1] = new Point(line1[0].X, line1[0].Y);
-                }
-                result[0] = new Point(result[1].X - Math.Abs(difference.X), result[1].Y - Math.Abs(difference.Y));
-            }
-            else
-            {
-                difference = new Point(line1[1].X - line2[1].X, line1[1].Y - line2[1].Y);
-                if (difference.X < 0 || difference.Y < 0)
-                {
-                    result[0] = new Point(line1[1].X, line1[1].Y);
-                }
-                else
-                {
-                    result[0] = new Point(line2[1].X, line2[1].Y);
-                }
-                result[1] = new Point(result[0].X + Math.Abs(difference.X), result[0].Y + Math.Abs(difference.Y));
-            }
-            return result;
-            
-        }
-
         public void Select(Color color)
         {
             if (!this._mActiveProvinces.Contains(color))
@@ -100,27 +66,6 @@ namespace EU4_Game_Editing_Tool_WinForm
             else
             {
                 this.mActivePath = null;
-            }
-        }
-
-        private class linesComparer : IEqualityComparer<Point[]>
-        {
-            public bool Equals(Point[] x, Point[] y)
-            {
-                if (x.Length > 2 || y.Length > 2)
-                {
-                    return false;
-                }
-                if ((x[0] != y[0]) || (x[1] != y[1]))
-                {
-                    return false;
-                }
-                return true;
-            }
-
-            public int GetHashCode(Point[] obj)
-            {
-                return obj[0].X * 2000 + obj[0].Y * 4000;
             }
         }
     }
